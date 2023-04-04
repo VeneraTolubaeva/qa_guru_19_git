22b50faba187e86b24c9188d90347b6449859e90
VeneraTolubaeva <venera.to@zohomail.com>
2023-04-04 15:28:03 +0300

    add getMessage

--- src/main/java/qaplanet/venera/Main.java
+++ src/main/java/qaplanet/venera/Main.java
@@ -2,6 +2,9 @@ package qaplanet.venera;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QAGURU!");
+        System.out.println(getMessage());
+    }
+    private static String getMessage(){
+        return "Hello QAGURU!";
     }
 }
