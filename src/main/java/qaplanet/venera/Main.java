2c3365779947704bf07d3525092f5bcf8fe77151
VeneraTolubaeva <89601828+VeneraTolubaeva@users.noreply.github.com>
2023-04-04 16:55:54 +0300

    Update Main.java

--- src/main/java/qaplanet/venera/Main.java
+++ src/main/java/qaplanet/venera/Main.java
@@ -2,10 +2,10 @@ package qaplanet.venera;
 
 public class Main {
     
-    private static String msg = "Hello qa.guru!";
+    private static String message = "Hello QAGURU!";
 
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(message);
     }
 
 }
